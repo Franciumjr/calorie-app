c2a7002a51e576a3982b8e2ee95c7b7907acc7fc
Franciumjr <123298452+Franciumjr@users.noreply.github.com>
2026-02-11 12:20:12 +0800

    Added comments on CardSkeleton component

--- src/components/CardSkeleton.jsx
+++ src/components/CardSkeleton.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const CardSkeleton = () => {
   return (
     <div className="p-6 bg-zinc-700/50 rounded-2xl flex flex-col items-center">
-    {/* Mimic the Image */}
+    {/*  image */}
     <Skeleton className="w-40 h-40 rounded-lg mb-4" />
     
-    {/* Mimic the Title */}
+    {/* title */}
     <Skeleton className="w-3/4 h-6 mb-4" />
     
-    {/* Mimic the Nutrients */}
+    {/*  nutrition value */}
     <div className="w-full space-y-2">
       <Skeleton className="w-full h-4" />
       <Skeleton className="w-full h-4" />
